0551d23a51ce99b48f6059ab6d074f6e12ff91ee
Michaeljbackman <153117265+Michaeljbackman@users.noreply.github.com>
2025-04-03 21:00:49 -0600

    idk

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ada3b5ff88830494853c8b93d3d11979d0708c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c35eac224318da4ca54a8a636701761a547dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
